6d4cdc712078e6a10614a2c4921fc8c41e337853
fzg1987 <beginer@yeaeh.net>
2020-10-23 21:45:31 +0800

    New Features spring courses

--- ch01-hellospring/src/test/java/com/fzg/MyTest.java
+++ ch01-hellospring/src/test/java/com/fzg/MyTest.java
@@ -6,6 +6,9 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.time.Clock;
+import java.util.Date;
+
 public class MyTest {
 
     @Test
@@ -13,6 +16,11 @@ public class MyTest {
         SomeService someService = new SomeServiceImpl();
         someService.doSome();
     }
+
+    /**
+     * spring默认创建对象的时间，是创建spring容器时，会创建配置文件中的所有对象。
+     * spring创建对象，默认创建的是无参数的对象
+     */
     @Test
     public void test02() {
         //使用spring容器创建的对象
@@ -28,5 +36,33 @@ public class MyTest {
         SomeService service = (SomeService) ac.getBean("someService");
         //使用spring创建好的对象
         service.doSome();
+    }
+    /**
+     *  获取spring容器中java对象的信息
+     */
+    @Test
+    public void test03() {
+        String config = "beans.xml";
+        ApplicationContext ac = new ClassPathXmlApplicationContext(config);
+        //使用spring中提供的方法，获取容器中定义的对象
+        int numbers = ac.getBeanDefinitionCount();
+        System.out.println("容器中定义的对象数量：" + numbers);
+        //容器中每个定义的对象的名称
+        String names[] = ac.getBeanDefinitionNames();
+        for(String name: names){
+            System.out.println(name);
+        }
+    }
+
+    /**
+     * 使用一个非自定义类创建的对象
+     */
+    @Test
+    public void test04() {
+        String config = "beans.xml";
+        ApplicationContext ac = new ClassPathXmlApplicationContext(config);
+        //使用getBean
+        Date my = (Date) ac.getBean("mydate");
+        System.out.println("Date:"+my);
     }
 }
